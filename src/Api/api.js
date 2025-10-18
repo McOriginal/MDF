201d0f81a66abd18c4e994544adc33144a91a4c9
McOriginal <originalmc223@gmail.com>
2025-10-18 07:15:55 +0000

    Ajout de API Backend

--- src/Api/api.js
+++ src/Api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backend-groupe-siby-banconi.onrender.com/api',
+  baseURL: 'https://backend-mdf.onrender.com/api',
   // 'http://localhost:5000/api',
 
   headers: {
